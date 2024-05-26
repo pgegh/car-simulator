bbf76ff7ed489682a567c6e4018bd2469ee3a16e
Hovig <hovig@mail.com>
2024-05-26 22:57:48 +0200

    Add function for getting room size

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+#include "room.h"
+#include "spatial.h"
+#include "utils.h"
+
 int main(void) {
     printf(
         "#########################################################\n"
         "####  WELCOME TO THE RADIO CONTROLLED CAR SIMULATOR  ####\n"
         "#########################################################\n\n");
+
+
+    const RoomSize_T room_size = get_room_size();
+    const Room_T room = create_room(&room_size);
+
+    printf("%d %d\n", room.room_size.x_size, room.room_size.y_size);
 
     printf(
         "#########################################################\n"
