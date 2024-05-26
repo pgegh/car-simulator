d14712d5c808f8ad41f357e39bd491782dc61ba8
Hovig <hovig@mail.com>
2024-05-26 22:40:02 +0200

    Create data structure for room and room size

--- room.h
+++ room.h
@@ -1,4 +1,15 @@
 #ifndef ROOM_H
 #define ROOM_H
 
+#include "spatial.h"
+
+/* Room data structure represtneing a room.  */
+typedef struct Room {
+    RoomSize_T room_size;
+} Room_T;
+
+/* Creates a new room instance.
+   Returns the room instance.  */
+Room_T create_room(const RoomSize_T *room_size);
+
 #endif //ROOM_H
