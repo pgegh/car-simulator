a0701c8e77f1ff476817c6f5c14f5251c7b29b51
Hovig <hovig@mail.com>
2024-05-27 01:07:12 +0200

    Implement executor and results printer

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "cars.h"
 #include "room.h"
@@ -17,17 +18,19 @@ int main(void) {
 
     const RoomSize_T room_size = get_room_size();
     const Room_T room = create_room(&room_size);
-    printf("%d %d\n", room.room_size.x_size, room.room_size.y_size);
 
     const PositionDirection_T position_direction = get_car_start_position_and_direction(&room_size);
     const Car_T car = create_car(MonsterTruck, 1u, &position_direction);
-    printf("%d %d %s\n", room.room_size.x_size, room.room_size.y_size, dir_to_str(car.direction));
 
     char commands_string[NUMBER_OF_COMMANDS_MAX + 1];
     get_commands_string(commands_string);
 
     Simulation_T *simulation = create_simulation(&room, &car, commands_string);
-    print_simulation_state_info(simulation);
+
+    const SimulationResults_T results = run_simulation(simulation);
+    print_simulation_results(&results);
+
+    free(simulation);
 
     printf(
         "#########################################################\n"
--- simulation.h
+++ simulation.h
@@ -12,11 +12,31 @@ typedef struct Simulation {
     unsigned int command_index;
 } Simulation_T;
 
+
 /* Malloc a simulation instance that contains all simulation state information.
    Returns a pointer to the simulation instance.  */
 Simulation_T *create_simulation(const Room_T *room, const Car_T *car, const char *commands_string);
 
+
 /* Prints a human readable representation of all data in the simulation instance.  */
 void print_simulation_state_info(const Simulation_T *simulation);
+
+
+/* Simulation result data structure which represents the results of a simulation run.  */
+typedef struct SimulationResults {
+    bool success;
+    Position_T car_final_position;
+    Direction_E car_final_direction;
+    unsigned int last_successful_command_position;
+} SimulationResults_T;
+
+
+/* Runs the simulation and returns the resulsts of the run.  */
+SimulationResults_T run_simulation(Simulation_T *simulation);
+
+
+/* Prints a human readable simulation results.  */
+void print_simulation_results(const SimulationResults_T *simulation_results);
+
 
 #endif //SIMULATION_H
